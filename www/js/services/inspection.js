8a10b03c75133c4bc964459ae7d5432e3a40d34a
Dmitri Skjorshammer <dmitriskj@gmail.com>
2017-02-02 23:57:51 -0800

    Update how we create documentID for inspection.

--- www/js/services/inspection.js
+++ www/js/services/inspection.js
@@ -8,22 +8,19 @@ https://www.firebase.com/docs/web/guide/login/password.html
 angular.module('starter.services')
 .factory('Inspection', function($http, User, Pouch, $q, Backoff) {
   var backoff = new Backoff({ min: 1000, max: 60000 });
-  var whitelistedKeys = ["id", "report", "color"];
+  var whitelistedKeys = ["id", "report", "color", "created_at"];
 
   // Pouch.inspectionsDB.destroy()
   return {
 
     documentID: function(location_doc_id, visit_doc_id, ins) {
-      console.log("-----")
-      console.log(location_doc_id)
-      console.log(visit_doc_id)
-      console.log(ins)
-      console.log("-----")
-      return location_doc_id + visit_doc_id + ins.report.breeding_site.description + ins.report.field_identifier + ins.report.report
+      return location_doc_id + visit_doc_id + ins.created_at
     },
 
     getAll: function(ins_doc_ids) {
-      return Pouch.inspectionsDB.allDocs({keys: ins_doc_ids})
+      return Pouch.inspectionsDB.allDocs({keys: ins_doc_ids, include_docs: true}).then(function(docs) {
+        return docs.rows.map(function(el) { return el.doc })
+      })
     },
 
     syncUnsyncedDocuments: function() {
