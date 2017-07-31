8dbf1e69b439ce36e09dc3ef498c316bd5a74c26
Dmitri Skjorshammer <dmitriskj@gmail.com>
2017-07-31 15:03:13 -0700

    Adds Questionnaire services.

---
+++ www/js/services/questionnaire.js
@@ -0,0 +1,31 @@
+/*
+This factory abstracts the interaction with the persistence layer to
+fetch the Patient object. The Patient object is *mostly* Firebase data that
+is returned via password authentication:
+
+https://www.firebase.com/docs/web/guide/login/password.html
+*/
+angular.module('starter.services')
+.factory('Questionnaire', function() {
+  return {
+    shouldDisplay: function(question, questions) {
+      if (question.code == "informed_consent")
+        return true
+
+      for (var i=0; i < questions.length; i++) {
+        if (questions[i].code == "informed_consent" && questions[i].answer == 1)
+          return false
+      }
+
+      if (!question.parent)
+        return true
+
+      for (var i=0; i < questions.length; i++) {
+        if (questions[i].code == question.parent.code)
+          parentQ = questions[i]
+      }
+
+      return question.parent.display.indexOf(parentQ.answer) != -1
+    }
+  };
+})
