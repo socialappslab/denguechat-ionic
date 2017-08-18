88807c02878e4fc999606a5545ef98d668f6086f
Dmitri Skjorshammer <dmitriskj@gmail.com>
2017-08-18 13:30:48 -0700

    Adds android log.

---
+++ androidlog.sh
@@ -0,0 +1,3 @@
+# tail -f /Users/dmitri/.Genymobile/Genymotion/deployed/Custom\ Phone\ -\ 7.0.0\ -\ API\ 24\ -\ 768x1280/logcat.txt
+# tail -f /Users/dmitri/.Genymobile/Genymotion/deployed/Google\ Pixel\ -\ 7.1.0\ -\ API\ 25\ -\ 1080x1920/logcat-xJ1885.txt
+tail -f /Users/dmitri/.Genymobile/Genymotion/deployed/Samsung\ Galaxy\ S7\ -\ 7.1.0\ -\ API\ 25\ -\ 1440x2560/logcat-xJ2828.txt
