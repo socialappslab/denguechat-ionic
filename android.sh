ab469130bb26c1b29346e352f5499142d50a597b
Dmitri Skjorshammer <dmitriskj@gmail.com>
2017-09-21 00:32:33 -0700

    Updates ./android.sh script.

--- android.sh
+++ android.sh
@@ -1,5 +1,15 @@
+# gulp production &&
+# cordova build --release android &&
+# jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /Users/dmitri/.android/google-play-app-signing.jks platforms/android/build/outputs/apk/android-release-unsigned.apk GooglePlayAppSigningKey &&
+# /Applications/adt-bundle-mac-x86_64-20140702/sdk/build-tools/25.0.0/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk DengueChat.apk
+# # Now, go to https://play.google.com/apps/publish
+
+# gulp production &&
+# cordova build --release android &&
+# jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /Users/dmitri/.android/google-play-app-signing.jks platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk GooglePlayAppSigningKey &&
+# /Applications/adt-bundle-mac-x86_64-20140702/sdk/build-tools/25.0.0/zipalign -v 4 platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk DengueChat-armv7.apk
+
 gulp production &&
 cordova build --release android &&
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /Users/dmitri/.android/google-play-app-signing.jks platforms/android/build/outputs/apk/android-release-unsigned.apk GooglePlayAppSigningKey &&
-/Applications/adt-bundle-mac-x86_64-20140702/sdk/build-tools/25.0.0/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk DengueChat.apk
-# Now, go to https://play.google.com/apps/publish
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /Users/dmitri/.android/google-play-app-signing.jks platforms/android/build/outputs/apk/android-x86-release-unsigned.apk GooglePlayAppSigningKey &&
+/Applications/adt-bundle-mac-x86_64-20140702/sdk/build-tools/25.0.0/zipalign -v 4 platforms/android/build/outputs/apk/android-x86-release-unsigned.apk DengueChat-x86.apk
