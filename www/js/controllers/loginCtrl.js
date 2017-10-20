0f0b5034b5e73d1e5711d14ff5674baf9ca97ee1
eferreira <eferreira@codium.com.py>
2017-10-20 15:01:16 -0300

    test using cordova-advanced-http plugin

--- www/js/controllers/loginCtrl.js
+++ www/js/controllers/loginCtrl.js
@@ -11,7 +11,7 @@ angular.module('starter.controllers')
 
   $scope.login = function(){
     $scope.state = {loading: true, error: null}
-
+/*
     User.session($scope.user.username, $scope.user.password).then(function(response) {
       return User.save(response.data.user).then(function(doc) {
         $scope.$emit(denguechat.env.auth.success, {})
@@ -20,7 +20,34 @@ angular.module('starter.controllers')
       $scope.$emit(denguechat.error, res)
     }).finally(function() {
       $scope.state.loading = false;
-    })
+    })*/
+
+    cordova.plugin.http.acceptAllCerts(true, function() {
+      console.log('SSL pinning success!');
+      console.log("request to: " + denguechat.env.baseURL + "sessions");
+      console.log("whit params: " + $scope.user.username + "/" + $scope.user.password)
+  
+    cordova.plugin.http.post(denguechat.env.baseURL + "sessions", {
+      username: $scope.user.username,
+      password: $scope.user.password
+    }, { Authorization: "OAuth2: token" }, function(response) {
+      // prints 200
+      console.log(response.status);
+      $scope.$emit(denguechat.env.auth.success, {})
+    }, function(response) {
+      // prints 403
+      console.log(response.status);
+  
+      //prints Permission denied
+      console.log(response.error);
+      console.log("Error: " + JSON.stringify(response));
+    });
+
+  }, function() {
+    console.log('error :(');
+});
+
+
   }
 
   $scope.signup = function(){
